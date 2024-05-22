15dceb4e6d32b986c8953740a000f2e908a19820
NgigiN <ngigi.nyongo@gmail.com>
2024-05-22 10:58:48 +0300

    Trigger for updating items after order

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,6 +1,7 @@
 -- This script creates a trigger that decreases an item's
 -- quantitiy after adding a new order
-CREATE TRIGGER quantity_decrease
+DROP TRIGGER IF EXISTS reduce_quantity;
+DELIMITER $$ CREATE TRIGGER reduce_quantity
 AFTER
 INSERT ON orders FOR EACH ROW BEGIN
 UPDATE items
