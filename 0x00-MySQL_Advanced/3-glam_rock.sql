236b3e69bfb9e9ca7670fbdef1ae6cb30e53e25c
NgigiN <ngigi.nyongo@gmail.com>
2024-05-22 10:31:48 +0300

    List all Glamrock bands by longetivity

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- This script should list all bands with Glam Rock
 -- their main style, ranked by longetivity
 SELECT band_name,
-  (IFNULL(split, '2020') - formed) AS lifespan
+  (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
 WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 ORDER BY lifespan DESC;
