6dbb91f3f996f09a3a5ff773d5de4415071438a3
NgigiN <ngigi.nyongo@gmail.com>
2024-05-22 09:20:44 +0300

    Calculating the fans by counrty of origin

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- This script ranks country of origin of bands
 -- ordered by the number of non-unique fans
 SELECT origin,
-  SUM(fans) AS total_fans
+  SUM(fans) AS nb_fans
 from metal_bands
 GROUP BY origin
-ORDER BY total_fans DESC;
+ORDER BY nb_fans DESC;
