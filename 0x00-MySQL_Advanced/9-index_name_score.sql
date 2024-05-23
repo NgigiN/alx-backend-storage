149265913880905039d2af4809618be1bd1e8749
NgigiN <ngigi.nyongo@gmail.com>
2024-05-23 13:49:56 +0300

    optimizing search and score

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
 -- This script creates an index on a table(names) and the first letter
 -- a column(name) and another column(score)
-CREATE INDEX idx_name_first_score on names(name(1), score(1));
+CREATE INDEX idx_name_first_score on names(name(1), score);
