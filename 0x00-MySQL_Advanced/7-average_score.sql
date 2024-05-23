bdf0e5e3c14c1b7ffaf59a8c7f7fe73a1e8dff84
NgigiN <ngigi.nyongo@gmail.com>
2024-05-23 13:43:11 +0300

    average score procedure

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,19 +5,19 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
     DECLARE total_score INT DEFAULT 0;
-    DECLARE project_count INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
 
     SELECT SUM(score)
-	INTO total_score
-	FROM corrections
-	WHERE corrections.user_id = user_id;
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
     SELECT COUNT(*)
-	INTO projects_count
-	from corrections
-	WHERE corrections.user_id = user_id;
+        INTO projects_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
 
     UPDATE users
-	SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
-	WHERE users.id = user_id;
+        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+        WHERE users.id = user_id;
 END $$
 DELIMITER ;
