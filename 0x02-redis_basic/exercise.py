67ed1c34b719d86d4f8dc5b61e7add84716168bc
NgigiN <ngigi.nyongo@gmail.com>
2024-05-27 15:05:44 +0300

    Storing Lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -36,7 +36,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
